09392509e1db3c1bd0618fd2c2f9b0dd52ec149b
Lorenzo Hucke <lorenzohucke@gmail.com>
2025-09-01 12:57:08 -0400

    Añadi barra navegacion, texto header, barra busqueda

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,22 @@ function App() {
     return (
         <div className="App">
             <header className="ejemplo">Encabezado</header>
-            <section className="ejemplo">Sección de búsqueda</section>
+                <nav>
+                    <a href ="buscar-todo">Buscar todo</a>
+                    <a href ="hoteles">Hoteles</a>
+                    <a href ="cosas-que-hacer">Cosas que hacer</a>
+                    <a href ="restaurantes">Restaurantes</a>
+                    <a href ="vuelos">Vuelos</a>
+                </nav>
+
+                <div id="dondeIr">
+                    <h1>¿Adonde quieres ir?</h1>
+                </div>
+
+            <section className="SeccionBusqueda">Sección de búsqueda</section>
+                    <input placeholder="Busca tu destino"/>
+                    <button className ="botonDeBusqueda">Buscar</button>
+
             <section className="ejemplo">Sección de catálogo</section>
             <section className="ejemplo">Sección de información</section>
             <footer className="ejemplo">Pie de página</footer>
