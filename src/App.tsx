f90053064c309f099714dba95f931a0d253d5e1c
Manuel Cristóbal Escobedo <manuelcristobalescobedo@gmail.com>
2025-09-02 15:43:32 -0400

    Addición de los elementos HTML del Encabezado

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import SeccionGaleria from "./components/SeccionGaleria";
 import Pie from "./components/Pie";
 import SeccionBusqueda from "./components/SeccionBusqueda";
 import SeccionInformacion from "./components/SeccionInformacion";
+import Encabezado from "./components/Encabezado";
 
 function App() {
 
     return (
         <>
         <div className="App">
+            <Encabezado />
             <SeccionBusqueda/>
         </div>
         <div>
