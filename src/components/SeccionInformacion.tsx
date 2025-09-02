d8ee2c911f8ad6237802340a393841bb4a456d10
Lorenzo Hucke <lorenzohucke@gmail.com>
2025-09-02 00:10:50 -0400

    Se añadio la seccion informacion + arreglo de nav header

--- src/components/SeccionInformacion.tsx
+++ src/components/SeccionInformacion.tsx
@@ -6,7 +6,22 @@ const SeccionInformacion: React.FC = () => {
 
 return (
         <>
-            <section className="SeccionInformacion">Sección de información</section>
+            <section className="SeccionInformacion"></section>
+
+            <div className = "Container">
+                <div className="InformacionRow">
+                    <h3>Actividades en todo Chile</h3>
+                    <p>Disfruta de cientos de actividades a lo largo del pais</p>
+                </div>
+                <div>
+                    <h3>Atencion al cliente 24/7</h3>
+                    <p>Gestiona tu reserva y resuelve dudas en todo momento</p>
+                </div>
+                <div>
+                    <h3>Confirmación inmediata</h3>
+                    <p>Recibe un correo confirmando tu pago y reserva de la actividad</p>
+                </div>
+            </div>
         </>
         )
     };
